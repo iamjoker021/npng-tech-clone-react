33bf35e76db572d2b19d2a85e742fd10c0308d39
iamjoker021 <iamjoker021@gmail.com>
2024-09-06 16:03:46 +0530

    chore: Add Hamburger in Mobuile view

---
+++ src/components/DesktopNav.jsx
@@ -0,0 +1,12 @@
+import NavList from "./NavList";
+
+const DesktopNav = () => {
+    return (
+        <nav className="desktop-navbar">
+            <NavList />
+        </nav>
+    )
+}
+
+
+export default DesktopNav;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,17 @@
-import NavList from "./NavList"
+import DesktopNav from "./DesktopNav"
+import MobileNav from "./MobileNav"
 
 const Header = () => {
     return (
         <header>
-        <div class="brand-logo">
-            <a href=""><img class="logo" src="https://npngtech.in/assets/large-DIFPjkXK.png" alt="npng-logo" /><h1 class="brand-name">NPNG Tech</h1></a>
-        </div>
+            <div class="brand-logo">
+                <a href=""><img class="logo" src="https://npngtech.in/assets/large-DIFPjkXK.png" alt="npng-logo" /><h1 class="brand-name">NPNG Tech</h1></a>
+            </div>
+            <DesktopNav />
+            <MobileNav />
+            <button>Sign In</button>
+        </header>
         
-        <nav>
-            <NavList />
-        </nav>
-        <button type="button">Sign Up</button>
-    </header>
     )
 }
 
